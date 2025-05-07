ba61ad76203728aabfa226485ec3b5e0922338da
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2025-05-07 14:39:34 -0700

    Remove 'export' from build

--- build.ts
+++ build.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S deno run -A
 
+// Read files
 const ts = Deno.readTextFileSync(import.meta.dirname + "/src/trace.ts");
 const css = Deno.readTextFileSync(import.meta.dirname + "/src/trace.css");
-Deno.writeTextFileSync("trace.js", ts.replace("/* CSS */", "\n" + css));
+
+// Remove all 'export' statements and replace CSS with style content
+Deno.writeTextFileSync("trace.js", ts.replace(/export\s+/, "").replace("/* CSS */", "\n" + css));
